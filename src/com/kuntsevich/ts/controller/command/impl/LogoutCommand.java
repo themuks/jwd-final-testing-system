2d2f50bda6cffa58c61e5431bc88dc9c0e39cb4f
Maksim Kuntsevich <max-cun4@yandex.ru>
2020-11-11 19:50:13 +0300

    fix: fix logout

--- src/com/kuntsevich/ts/controller/command/impl/LogoutCommand.java
+++ src/com/kuntsevich/ts/controller/command/impl/LogoutCommand.java
@@ -5,18 +5,26 @@ import com.kuntsevich.ts.controller.SessionAttribute;
 import com.kuntsevich.ts.controller.command.Command;
 import com.kuntsevich.ts.controller.router.Router;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class LogoutCommand implements Command {
+
+    private static final String USER_HASH = "userHash";
+    private static final String EMPTY_STRING = "";
+    private static final String USER_EMAIL = "userEmail";
+
     @Override
     public Router execute(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
-        session.removeAttribute(SessionAttribute.ROLE);
-        session.removeAttribute(SessionAttribute.USER_ID);
-        session.removeAttribute(SessionAttribute.USER_HASH);
         session.invalidate();
+        Cookie cookie = new Cookie(USER_HASH, EMPTY_STRING);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        cookie = new Cookie(USER_EMAIL, EMPTY_STRING);
+        response.addCookie(cookie);
         return new Router(PagePath.HOME);
     }
 }
